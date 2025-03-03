fbcd61ac294dcb88fda6c1d380675580a8275970
giordanofabrizii <giordanofabrizi@gmail.com>
2025-03-03 14:05:38 +0100

    added display of the max value on the counter

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("ciao")
+const counterEl = document.getElementById("counter"); // counter element on the DOM
+const maxValue = 1900; // value at which the counter have to stop
+var counterValue = 0; // value of the counter at the beginning
+
+for (let i = 0; i <= maxValue; i++) {
+    counterEl.innerText = i;
+}
