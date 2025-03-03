153701402790374cd426d7008289b7a710fa31ee
giordanofabrizii <giordanofabrizi@gmail.com>
2025-03-03 14:16:03 +0100

    added increasing of the value

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const counterEl = document.getElementById("counter"); // counter element on the DOM
-const maxValue = 1900; // value at which the counter have to stop
+const maxValue = 200; // value at which the counter have to stop
 var counterValue = 0; // value of the counter at the beginning
 
-for (let i = 0; i <= maxValue; i++) {
-    counterEl.innerText = i;
-}
+let interval = setInterval(() => {
+    if (counterValue <= maxValue) {
+        counterEl.innerText = counterValue;
+        counterValue++
+    } else {
+        clearInterval(interval);
+    }
+}, 2); // ! set interval for 2 seconds
